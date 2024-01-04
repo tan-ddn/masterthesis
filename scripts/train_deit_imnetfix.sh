f7911d822d24c4a8fc3ae1125419331c7008a861
Tan Nguyen <tnguyen@lfb.rwth-aachen.de>
2024-01-04 21:44:13 +0100

    Update sh and tbi files

--- scripts/train_deit_imnetfix.sh
+++ scripts/train_deit_imnetfix.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/students/tnguyen/masterthesis/scripts
 chmod +x train_deit_imnetfix.sh
-/home/students/tnguyen/miniconda3/envs/4p2p_env/bin/python -m torch.distributed.launch --nproc_per_node=4 --use_env --master_port=25682 /home/students/tnguyen/masterthesis/deit/main.py "$@"
+/home/students/tnguyen/miniconda3/envs/4p2p_env/bin/python -m torch.distributed.launch --nproc_per_node=4 --use_env --master_port=22201 /home/students/tnguyen/masterthesis/deit/main.py "$@"
